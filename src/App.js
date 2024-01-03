bba8e87f0046fed9ec8a52f9a501c30cbfecd994
matteussaraujo <mateusdev36@gmail.com>
2024-01-02 22:34:32 -0300

    feat: creation of the row component, showing movies from the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import categories from './api';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
-      
+     {categories.map((category) => {
+     return(
+     <Row 
+     key={category.name} 
+     title ={category.title} 
+     path={category.path}
+     />);
+     })} 
     </div>
   );
 }
